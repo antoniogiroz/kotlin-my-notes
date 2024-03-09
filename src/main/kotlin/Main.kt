f6d93007d8d8ea36d2e4f40df9bac6782d31b9bc
Antonio Giroz <antonioluisgil@gmail.com>
2024-03-09 20:51:33 +0100

    chore: create UI for Notes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,19 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -16,24 +21,45 @@ import androidx.compose.ui.window.application
 @Preview
 fun App(appState: AppState) {
     MaterialTheme {
-        Column {
-            TextField(value = appState.text.value, onValueChange = { nextValue -> appState.text.value = nextValue })
-            Text(text = appState.message)
-            Button(onClick = { appState.text.value = "" }, enabled = appState.buttonEnabled) {
-                Text("Clean")
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(appState.notes.value) { note ->
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(.8f)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Row {
+                            Text(
+                                text = note.title,
+                                style = MaterialTheme.typography.h6,
+                                modifier = Modifier.weight(1f)
+                            )
+                            if (note.type == Note.Type.AUDIO) {
+                                Icon(
+                                    imageVector = Icons.Default.Mic,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = note.description
+                        )
+                    }
+                }
             }
         }
-
     }
 }
 
 class AppState {
-    var text = mutableStateOf("")
-    val message
-        get() = "Hello ${text.value}"
-
-    val buttonEnabled
-        get() = text.value.isNotEmpty()
+    var notes = mutableStateOf(getNotes())
 }
 
 
